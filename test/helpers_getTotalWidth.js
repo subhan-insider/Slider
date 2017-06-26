b27d794b6f8818796ccd8245999c6d6e74ba34ea
M Subhan Naeem <sub.metu@gmail.com>
2017-06-26 16:59:26 +0400

    updates and clean up

--- test/helpers_getTotalWidth.js
+++
@@ -1,7 +0,0 @@
-var getTotalWidth = function( pages, marginLeft, marginRight ){
-    var totalWidth = 0;
-    pages.each(function (index, page) {
-        totalWidth += $(page).width() + marginRight + marginLeft;
-    });
-    return totalWidth;
-}
